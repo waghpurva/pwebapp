f7f2ec6d40a159398fa199d28b43b725190e0ed9
waghpurva <71317136+waghpurva@users.noreply.github.com>
2023-05-09 19:03:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 
 
 app.get('/hello', function (req, res) {
-    var person={firstName:'Ravi',lastName:'Tambade',age:47};
+    var person={firstName:'Purva',lastName:'Wagh',age:22};
     res.send(person);
 });
 
